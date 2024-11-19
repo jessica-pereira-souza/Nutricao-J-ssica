048f36e70925d7c3fe95e1586dc4c24147c9923f
jessica-pereira-souza <126929259+jessica-pereira-souza@users.noreply.github.com>
2024-11-19 00:22:24 +0000

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var titulo = document.querySelector ("h1");
+titulo.textContent = "Jéssica Nutrição";
+
+var subtitulo = document.querySelector ("h2");
+subtitulo.textContent = "Pacientes cadastrados";
+
+var paciente = document.querySelector ("#primeiro-paciente");
+console.log(paciente);
